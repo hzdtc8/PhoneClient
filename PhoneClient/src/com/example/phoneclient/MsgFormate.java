88881845b149ca754e964338195de734e75aed81
Zheng Huang <zheng.huang@ndsu.edu>
2015-06-15 15:58:24 -0500

    Finished message formate and setter and getter

--- PhoneClient/src/com/example/phoneclient/MsgFormate.java
+++ PhoneClient/src/com/example/phoneclient/MsgFormate.java
@@ -12,7 +12,33 @@ public class MsgFormate {
 	
 	}
 	
-	private static String setup ="setup;{0};{1};{2}/>"; //setup;tagID;caption;content/>
+	private static String setupd="setup;{0};{1};{2}/>"; //setup;tagID;caption;content/>
 	private static String crossDevice="crossDevice;{0};{1};{2}/>";//crossDevice;tagID;caption;content/>
+	private static String objectClick="objectClick;{0};{1};{2}/>";//objectClick;tagID;caption;content/>
+	private static String returnValue="returnValue;{0};{1};{2}/>";//returnValue;tagID;caption;content/>
+	private static String returnGesture="returnGesture;{0};{1};{2}/>";//returnGesture;tagID;caption;content/>
+	
+	private String messageText;
+	private MsgType messageType;
+	
+	public String getMessageText()
+	{
+		return messageText;
+	}
+	public void setMessageText(String text)
+	{
+		messageText=text;
+	}
+	public MsgType getMessageType()
+	{
+		return messageType;
+	}
+	public void setMessageType(MsgType msgType)
+	{
+		messageType=msgType;
+	}
+	
+	
+
 
 }
